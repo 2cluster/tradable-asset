77587b9dfb078c4d42a259a3d507d549ec9e84b1
vagrant <vagrant@vagrant.vm>
2021-02-13 18:28:47 +0000

    copied from fabric samples

---
+++ main.go
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: Undefined
+
+
+package main
+
+import (
+	"log"
+
+	"github.com/hyperledger/fabric-contract-api-go/contractapi"
+	"github.com/2cluster/tradable-asset/chaincode"
+)
+
+func main() {
+	assetChaincode, err := contractapi.NewChaincode(&chaincode.SmartContract{})
+	if err != nil {
+		log.Panicf("Error creating asset-transfer-basic chaincode: %v", err)
+	}
+
+	if err := assetChaincode.Start(); err != nil {
+		log.Panicf("Error starting asset-transfer-basic chaincode: %v", err)
+	}
+}
